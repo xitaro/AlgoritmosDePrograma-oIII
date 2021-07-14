38dafa534934c81965ddbeda236fae4976f54cb0
Cassiano-code <cassianotr@gmail.com>
2021-07-14 15:10:32 -0300

    a

--- TrabalhoFinal/Main.py
+++
@@ -1,9 +0,0 @@
-import collections
-from Huffman import Huffman
-
-file = open('E:\Aulas\Algoritmos de Programação\Python\Algoritmos de Programação 3\Aula 18\hino.txt','r', encoding="utf8")
-read = file.read()
-
-huffman = Huffman()
-
-huffman.Frequency(read)
